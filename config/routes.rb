d1db56af50d0ea4c3d99363998fa4421ffd982f3
Amelia Galgon <agalgon@gmail.com>
2018-02-14 13:59:12 -0500

    removed refund controller and added destroy actions to charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   resources :wikis
-  resources :charges, only: [:new, :create]
-  resources :refunds, only: [:new, :create]
+  resources :charges, only: [:new, :create, :destroy]
 
   devise_for :users
   get 'about' => 'welcome#about'
